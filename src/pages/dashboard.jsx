ec32e74bf01adf695f9c79c6024898dccef0ea0c
haseeb11634 <haseeb11634@gmail.com>
2023-02-12 23:15:35 +0500

    added card design initial

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -4,7 +4,7 @@ export default function Dashboard() {
   return (
     <>
       <div>Dashboard</div>
-      <div>
+      <div style={{ textAlign: 'justify' }}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio quaerat nam, velit alias exercitationem sit rem
         soluta. Vitae cupiditate ratione in dolores harum quas deleniti! Molestias ab nam odit in! Lorem ipsum dolor sit amet
         consectetur adipisicing elit. Distinctio quaerat nam, velit alias exercitationem sit rem soluta. Vitae cupiditate ratione
--- src/pages/workflow.jsx
+++ src/pages/workflow.jsx
@@ -1,9 +1,112 @@
 import React from 'react';
+import styles from '@/styles/workflow.module.scss';
+import Image from 'next/image';
 
 export default function workflow() {
   return (
     <>
-      <div>workflow</div>
+      <div className={styles.grid_container}>
+        <div className={styles.grid_item}>
+          <div className={styles.grid_item_header}>
+            <div className={styles.grid_item_header_left}>
+              <span>Backlog(2)</span>
+              <Image src='/header-svg/filter.svg' alt='filter' width={16} height={16} />
+              <Image src='/header-svg/sortBy.svg' alt='sortBy' width={16} height={16} />
+            </div>
+            <div>
+              <Image src='/svg/menu.svg' alt='menu' width={24} height={24} />
+            </div>
+          </div>
+
+          <div className={styles.card}>
+            <div className={styles.card_header}>
+              <div className={styles.menu_icon}></div>
+              <h3 className={styles.title}></h3>
+            </div>
+            <div className={styles.card_body}></div>
+          </div>
+        </div>
+        <div className={styles.grid_item}>
+          <div className={styles.grid_item_header}>
+            <div className={styles.grid_item_header_left}>
+              <span>Backlog(2)</span>
+              <Image src='/header-svg/filter.svg' alt='filter' width={16} height={16} />
+              <Image src='/header-svg/sortBy.svg' alt='sortBy' width={16} height={16} />
+            </div>
+            <div>
+              <Image src='/svg/menu.svg' alt='menu' width={24} height={24} />
+            </div>
+          </div>
+
+          <div className={styles.card}>
+            <div className={styles.card_header}>
+              <div className={styles.menu_icon}></div>
+              <h3 className={styles.title}></h3>
+            </div>
+            <div className={styles.card_body}></div>
+          </div>
+        </div>
+        <div className={styles.grid_item}>
+          <div className={styles.grid_item_header}>
+            <div className={styles.grid_item_header_left}>
+              <span>Backlog(2)</span>
+              <Image src='/header-svg/filter.svg' alt='filter' width={16} height={16} />
+              <Image src='/header-svg/sortBy.svg' alt='sortBy' width={16} height={16} />
+            </div>
+            <div>
+              <Image src='/svg/menu.svg' alt='menu' width={24} height={24} />
+            </div>
+          </div>
+
+          <div className={styles.card}>
+            <div className={styles.card_header}>
+              <div className={styles.menu_icon}></div>
+              <h3 className={styles.title}></h3>
+            </div>
+            <div className={styles.card_body}></div>
+          </div>
+        </div>
+        <div className={styles.grid_item}>
+          <div className={styles.grid_item_header}>
+            <div className={styles.grid_item_header_left}>
+              <span>Backlog(2)</span>
+              <Image src='/header-svg/filter.svg' alt='filter' width={16} height={16} />
+              <Image src='/header-svg/sortBy.svg' alt='sortBy' width={16} height={16} />
+            </div>
+            <div>
+              <Image src='/svg/menu.svg' alt='menu' width={24} height={24} />
+            </div>
+          </div>
+
+          <div className={styles.card}>
+            <div className={styles.card_header}>
+              <div className={styles.menu_icon}></div>
+              <h3 className={styles.title}></h3>
+            </div>
+            <div className={styles.card_body}></div>
+          </div>
+        </div>
+        <div className={styles.grid_item}>
+          <div className={styles.grid_item_header}>
+            <div className={styles.grid_item_header_left}>
+              <span>Backlog(2)</span>
+              <Image src='/header-svg/filter.svg' alt='filter' width={16} height={16} />
+              <Image src='/header-svg/sortBy.svg' alt='sortBy' width={16} height={16} />
+            </div>
+            <div>
+              <Image src='/svg/menu.svg' alt='menu' width={24} height={24} />
+            </div>
+          </div>
+
+          <div className={styles.card}>
+            <div className={styles.card_header}>
+              <div className={styles.menu_icon}></div>
+              <h3 className={styles.title}></h3>
+            </div>
+            <div className={styles.card_body}></div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
